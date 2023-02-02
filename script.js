44ede0e951e68a8f347ac7e429129b4c542fc8a6
Astha <aasudagreat@gmail.com>
2023-02-02 17:13:57 +0545

    js 2nd part

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 let sumOfCards=document.getElementById("sum")
 let showCards=document.getElementById("cards")
 let gameStatus=document.getElementById("game-status")
-
-
+let blackJack=false
+let isAlive=false
+let cardArray=[]
 
 function randomNumber(){
     let number= Math.floor(Math.random()*13)+1
@@ -14,25 +15,42 @@ function randomNumber(){
         return number
     }
 }
-
-
 function startGame(){
+    isAlive=true
     firstCard=randomNumber()
     secondCard=randomNumber()
-    let cardArray=[firstCard, secondCard]
-    // for (i=0; i<cardArray.length; i++){
-    //     let sum=0
-         sum=cardArray[0]+cardArray[1]
-         sumOfCards.textContent+=sum
-         showCards.textContent+= " " + cardArray[0] + " " + cardArray[1]
-         if (sum<21){
-            gameStatus.textContent="Do you want a new card?"
-         }
-         else if (sum===21){
-            gameStatus.textContent="You have got a blackjack"
-         }
-         else{
-            gameStatus.textContent="Sorry, you are out of the game!"
-         }
+    cardArray=[firstCard, secondCard]
+    sum=firstCard + secondCard
+    renderGame()}
 
-}
+    function renderGame(){
+        showCards.textContent="Cards: "
+        for (i=0; i<cardArray.length; i++){
+            showCards.textContent+= cardArray[i] + " " }
+           sumOfCards.textContent="Sum: " + sum 
+            if (sum<21){
+               gameStatus.textContent="Do you want a new card?"
+            }
+            else if (sum===21){
+               gameStatus.textContent="You have got a blackjack"
+               blackJack=true
+            }
+            else{
+               gameStatus.textContent="Sorry, you are out of the game!"
+               isAlive=false
+            }
+    }
+    
+    
+
+
+    function newGame(){
+        if (isAlive===true && blackJack===false) {
+            let card=randomNumber()
+            cardArray.push(card)
+            sum+=card
+            renderGame()
+        }
+        
+    }
+
